70901bd7237aa2e4922acd62dda4445ffb908af8
abdullah kuzhan <kuzhanabdullah@gmail.com>
2016-05-23 10:50:06 +0100

    Add project files.

---
+++ AspNet4.5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNet4._5.Startup))]
+namespace AspNet4._5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
